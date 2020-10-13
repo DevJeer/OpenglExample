6c23715c5d1a4f260ceb0c616304d9a1f9282b56
houjf <18829536923@163.com>
2020-10-13 18:47:56 +0800

    第1章：Windows基础 添加 1. 搭建Win32程序 2. 最简单的Win32程序

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+
+// WINAPI	 函数的调用方式
+// 这些变量都是应用程序启动时，OS传入的
+// hInstance	 表示当前应用程序的实例
+// hPrevInstance 表示上一次应用程序启动的实例
+// lpCmdLine	 表示通过命令行启动应用程序传递的参数
+// nShowCmd		 表示如何显示窗口
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	return 0;
+}
