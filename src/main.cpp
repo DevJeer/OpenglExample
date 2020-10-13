346a6fd42323a01f95568a0286ac5ab9175fdfa8
houjf <18829536923@163.com>
2020-10-13 19:04:58 +0800

    注册窗口

--- src/main.cpp
+++ src/main.cpp
@@ -8,5 +8,28 @@
 // nShowCmd		 表示如何显示窗口
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+	// 定义窗口的类
+	WNDCLASSEX wndClass;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.cbWndExtra = 0;
+	wndClass.hbrBackground = NULL;
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hIcon = NULL;
+	wndClass.hIconSm = NULL;
+	wndClass.hInstance = hInstance;
+	wndClass.lpfnWndProc = NULL;
+	wndClass.lpszClassName = "GLWindow";
+	wndClass.lpszMenuName = NULL;
+	wndClass.style = CS_VREDRAW | CS_HREDRAW;
+
+	// 注册窗口
+	ATOM atom = RegisterClassEx(&wndClass);
+	if (!atom)
+	{
+		MessageBox(NULL, "Notice", "Error", MB_OK);
+		return 0;
+	}
+
 	return 0;
 }
