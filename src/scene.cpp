e9dfb39db9ff10d6b06e44efbcea59e447192aea
DevJeer <1761569049@qq.com>
2020-10-15 19:38:56 +0800

    1.设置矩阵（mvp）

--- src/scene.cpp
+++ src/scene.cpp
@@ -1,7 +1,12 @@
 #include "scene.h"
 void Init()
 {
-	
+	// 设置当前投影矩阵的
+	glMatrixMode(GL_PROJECTION);
+	gluPerspective(50.0f, 800.0f / 600.0f, 0.1f, 1000.0f);
+	// 设置当前模型视口矩阵
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 void Draw()
