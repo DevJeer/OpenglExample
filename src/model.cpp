82e611ef32db80e5d4b312a327d273ad0f4f3aa6
DevJeer <1761569049@qq.com>
2020-10-26 19:51:19 +0800

    解码绘制指令

--- src/model.cpp
+++ src/model.cpp
@@ -12,6 +12,12 @@ void Model::Init(const char *modelPath)
 	struct FloatData {
 		float v[3];
 	};
+	// 定义绘制所需的结构体
+	struct VertexDefine {
+		int posIndex;
+		int texcoordIndex;
+		int normalIndex;
+	};
 
 	int nFileSize = 0;
 	unsigned char *fileContent = LoadFileContent(modelPath, nFileSize);
@@ -19,6 +25,9 @@ void Model::Init(const char *modelPath)
 		return;
 	// 保存位置 纹理坐标 法线
 	std::vector<FloatData> positions, texcoords, normals;
+	// 保存绘制所需要的信息
+	std::vector<VertexDefine> vertexes;
+	std::vector<int> indexes;
 
 	// 将加载的文件放在流中
 	std::stringstream ssFileContent((char*)fileContent);
@@ -66,7 +75,50 @@ void Model::Init(const char *modelPath)
 				}
 			}// face
 			else if (szOneLine[0] == 'f') {
-				printf("draw command : %s\n", szOneLine);
+				// 定义一个流对象
+				std::stringstream ssOneLine(szOneLine);
+				ssOneLine >> temp;
+				std::string vertexStr;
+				// 读取每行的数据  假设是三角形，每行都有三个顶点
+				for (int i = 0; i < 3; i++)
+				{
+					// 数据格式是 1/1/1 2/2/2 3/3/3
+					ssOneLine >> vertexStr;
+					size_t pos = vertexStr.find_first_of('/');
+					std::string posIndexStr = vertexStr.substr(0, pos);
+					// 读取纹理坐标的信息
+					size_t pos2 = vertexStr.find_first_of('/', pos + 1);
+					std::string texcoordIndexStr = vertexStr.substr(pos + 1, pos2 - 1 - pos);
+					// 读取法线的信息
+					std::string normalIndexStr = vertexStr.substr(pos2 + 1, vertexStr.length() - 1 - pos2);
+					// 转换为int类型
+					VertexDefine vd;
+					vd.posIndex = atoi(posIndexStr.c_str());
+					vd.texcoordIndex = atoi(texcoordIndexStr.c_str());
+					vd.normalIndex = atoi(normalIndexStr.c_str());
+
+
+					int nCurrentVertexIndex = -1;
+					int nCurrentVertexCount = (int)vertexes.size();
+					// 查找是否有重复的点
+					for (int j = 0; j < nCurrentVertexCount; j++)
+					{
+						if (vertexes[j].posIndex == vd.posIndex &&
+							vertexes[j].texcoordIndex == vd.texcoordIndex &&
+							vertexes[j].normalIndex == vd.normalIndex)
+						{
+							nCurrentVertexIndex = j;
+							break;
+						}
+					}
+					// 不相同的话，就添加这个点
+					if (nCurrentVertexIndex == -1)
+					{
+						nCurrentVertexIndex = (int)vertexes.size();
+						vertexes.push_back(vd);
+					}
+					indexes.push_back(nCurrentVertexIndex);
+				}
 			}
 		}
 	}
