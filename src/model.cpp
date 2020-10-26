13a2d8806b7bab7ca71863841abf787137f00608
DevJeer <1761569049@qq.com>
2020-10-26 19:11:30 +0800

    解析模型数据

--- src/model.cpp
+++ src/model.cpp
@@ -8,12 +8,21 @@ Model::Model()
 
 void Model::Init(const char *modelPath)
 {
+	// 定义保存顶点数据的结构体
+	struct FloatData {
+		float v[3];
+	};
+
 	int nFileSize = 0;
 	unsigned char *fileContent = LoadFileContent(modelPath, nFileSize);
 	if (fileContent == nullptr)
 		return;
+	// 保存位置 纹理坐标 法线
+	std::vector<FloatData> positions, texcoords, normals;
+
 	// 将加载的文件放在流中
 	std::stringstream ssFileContent((char*)fileContent);
+	std::string temp;
 	char szOneLine[256];
 	while (!ssFileContent.eof())
 	{
@@ -26,14 +35,34 @@ void Model::Init(const char *modelPath)
 			// 纹理坐标		vt
 			// 法线坐标		vn	
 			if (szOneLine[0] == 'v') {
+				// 定义一个流对象
+				std::stringstream ssOneLine(szOneLine);
 				if (szOneLine[1] == 't') {
-					printf("texcoord : %s\n", szOneLine);
+					ssOneLine >> temp;
+					// 读取纹理坐标的信息
+					FloatData floatData;
+					ssOneLine >> floatData.v[0];
+					ssOneLine >> floatData.v[1];
+					texcoords.push_back(floatData);
+					printf("texcoord : %f, %f\n", floatData.v[0], floatData.v[1]);
 				}
 				else if (szOneLine[1] == 'n') {
-					printf("normal : %s\n", szOneLine);
+					ssOneLine >> temp;
+					// 读取法线的信息
+					FloatData floatData;
+					ssOneLine >> floatData.v[0];
+					ssOneLine >> floatData.v[1];
+					ssOneLine >> floatData.v[2];
+					printf("normal : %f, %f, %f\n", floatData.v[0], floatData.v[1], floatData.v[2]);
 				}
 				else {
-					printf("position : %s\n", szOneLine);
+					ssOneLine >> temp;
+					// 读取位置信息
+					FloatData floatData;
+					ssOneLine >> floatData.v[0];
+					ssOneLine >> floatData.v[1];
+					ssOneLine >> floatData.v[2];
+					printf("position : %f, %f, %f\n", floatData.v[0], floatData.v[1], floatData.v[2]);
 				}
 			}// face
 			else if (szOneLine[0] == 'f') {
